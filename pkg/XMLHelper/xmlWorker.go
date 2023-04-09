bcd8061e2296fb16191b26ec05312f0ac4cd24a2
i.chapaev <i.chapaev@smartway.today>
2023-04-10 00:55:36 +0300

    get xml header

--- pkg/XMLHelper/xmlWorker.go
+++ pkg/XMLHelper/xmlWorker.go
@@ -11,6 +11,8 @@ func GetXmlFile(employees []*Model.Employee) (*Model.File, error) {
 		Employees []*Model.Employee `xml:"employee"`
 		XMLName   xml.Name          `xml:"Employees"`
 	}{Employees: employees}
+
 	data, err := xml.MarshalIndent(tmp, "", "   ")
-	return &Model.File{FileName: "file.xml", Data: data, InsertDate: time.Now()}, err
+	responseData := []byte(xml.Header + string(data))
+	return &Model.File{FileName: "file.xml", Data: responseData, InsertDate: time.Now()}, err
 }
